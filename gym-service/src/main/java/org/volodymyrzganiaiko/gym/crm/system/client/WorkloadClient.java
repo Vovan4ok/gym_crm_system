49f6c95eec84d0f54bd060e6d5341367f40f9d53
Volodymyr Zganiaiko <vladimirzganiaiko@gmail.com>
2026-08-19 14:52:28 +0300

    - Add circuit breaker to the workload client

--- gym-service/src/main/java/org/volodymyrzganiaiko/gym/crm/system/client/WorkloadClient.java
+++ gym-service/src/main/java/org/volodymyrzganiaiko/gym/crm/system/client/WorkloadClient.java
@@ -3,9 +3,10 @@ package org.volodymyrzganiaiko.gym.crm.system.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.volodymyrzganiaiko.gym.crm.system.client.impl.WorkloadClientFallback;
 import org.volodymyrzganiaiko.gym.crm.system.dto.TrainerWorkloadRequest;
 
-@FeignClient(name="workload-service")
+@FeignClient(name="workload-service", fallback = WorkloadClientFallback.class)
 public interface WorkloadClient {
     @PostMapping("/api/workload")
     void sendWorkload(@RequestBody TrainerWorkloadRequest request);
