328a4b587ac4beffcd7fa255756364a4a89ab736
Volodymyr Zganiaiko <vladimirzganiaiko@gmail.com>
2026-09-01 15:28:13 +0300

    - Add API gateway module with static routing

---
+++ gateway-service/src/test/java/org/volodymyrzganiaiko/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.volodymyrzganiaiko.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
